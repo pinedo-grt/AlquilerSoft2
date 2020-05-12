6263df5d58b9acffbaae02215807335b777e2fd4
pinedo-grt <59301602+pinedo-grt@users.noreply.github.com>
2020-05-11 21:10:35 -0500

    SUBIDA DE PROYECTO
    Primer avance de codigo de AlquilerSoft

---
+++ AlquilerSoft.CapaPersistencia/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerSoft.CapaPersistencia
+{
+    public class Class1
+    {
+    }
+}
